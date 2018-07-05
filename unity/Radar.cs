43826b7fd435be1eb8d1ab32a72ab5f130200df1
Aso Takamichi <unk2072@gmail.com>
2018-07-05 19:24:50 +0900

    レーダー範囲外の場合に端に張り付くように修正

--- unity/Radar.cs
+++ unity/Radar.cs
@@ -16,26 +16,19 @@ public class Radar : MonoBehaviour {
         enemy = GameObject.FindGameObjectWithTag("Enemy");
         icon = GameObject.FindGameObjectWithTag("Icon");
 
-        // アイコンを非表示
-        icon.SetActive(false);
         // レーダー半径を取得
-        radarRadius = GetComponent<RectTransform>().sizeDelta.x / 2.0f;
+        radarRadius = GetComponent<RectTransform>().sizeDelta.x / 2.0f - 5;
     }
 
     // Update is called once per frame
     private void Update() {
-        // アイコンを非表示
-        icon.SetActive(false);
-
-        // アイコンの位置を計算して範囲内なら表示
-        if (Vector3.Distance(player.transform.position, enemy.transform.position) < detectionRange) {
-            icon.SetActive(true);
-            // 相対位置を計算
-            Vector3 pos = (enemy.transform.position - player.transform.position) * (radarRadius / detectionRange);
-            // カメラ角度に合わせて回転
-            pos = Quaternion.Euler(0, -Camera.main.gameObject.transform.eulerAngles.y, 0) * pos;
-            // 相対位置をレーダに反映
-            icon.GetComponent<RectTransform>().anchoredPosition = new Vector2(pos.x, pos.z);
-        }
+        // 相対位置を計算
+        Vector3 pos = (enemy.transform.position - player.transform.position) * (radarRadius / detectionRange);
+        // カメラ角度に合わせて回転
+        pos = Quaternion.Euler(0, -Camera.main.gameObject.transform.eulerAngles.y, 0) * pos;
+        // レーダー半径の範囲内に移動
+        pos = Vector3.ClampMagnitude(pos, radarRadius);
+        // 相対位置をレーダに反映
+        icon.GetComponent<RectTransform>().anchoredPosition = new Vector2(pos.x, pos.z);
     }
 }
